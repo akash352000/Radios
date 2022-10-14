12b34952c168d37d672602bfcfad02d92e685c94
akash352000 <akashvaddoriya007@gmail.com>
2022-10-14 13:08:03 -0400

    Radios

---
+++ Models/Radios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Radios.Models
+{
+    public class Radios
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime LaunchDate { get; set; }
+        public string company { get; set; }
+        public decimal Price { get; set; }
+        public string type { get; set; }
+        public decimal Generation { get; set; }
+
+    }
+}
---
+++ README.txt
@@ -0,0 +1,15 @@
+﻿Author: Akashkumar Vaddoriya
+Student ID: 0796076
+Date: 2022-101-14
+Project: About Radios
+
+103 I started making assignment
+103 I created new project and named it Radios
+104 I created Radios.cs file in Models
+105 I added & properties in that file
+
+105 Woo!! I didn't forgot to make README.
+106 I created README.txt file in RAdios 
+
+107 I created github repository 
+107 I push all this things to github
